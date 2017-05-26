a5fdacd493be763bf720b8f45c7c3628b54000a2
Hardus van der Berg <hardusvdberg@gmail.com>
2017-05-26 13:37:37 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -10,7 +10,7 @@ from datetime import datetime
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6'}
 
-genres = ('folk') #heavy', 'black', 'death', 'doom', 'thrash', 'speed', 'folk', 'power', 'prog', 'electronic', 'gothic', 'orchestral', 'avantgarde')
+genres = ('heavy', 'black', 'death', 'doom', 'thrash', 'speed', 'folk', 'power', 'prog', 'electronic', 'gothic', 'orchestral', 'avantgarde')
 
 genre_root = 'http://www.metal-archives.com/browse/ajax-genre/g/'
 
